07a1d36a8a59443d0eeef0ee699005d84d8e6814
AnaMarquez <anamarquez.edu@hotmail.com>
2020-05-23 15:44:35 -0700

    Slildeshow page

---
+++ 03/app.js
@@ -0,0 +1,14 @@
+const heading ='Hola nosotros somos..';
+let i = 0;
+
+
+console.log(heading.length);
+const typing = () =>{
+    if(i < heading.length){
+        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        i++;
+        setTimeout(typing,150);
+    }
+}
+
+typing();
